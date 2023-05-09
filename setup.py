963616b8316b36ed8f22214f571d715419498d0f
Liubov Nedoshivina <31125474+nedshivina@users.noreply.github.com>
2023-05-09 13:40:50 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,5 @@ from setuptools import setup  # type: ignore
 from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
-__version__ = "0.0.1"
 
-setup(long_description=long_description, version=__version__)
+setup(long_description=long_description)
