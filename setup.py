79ac3c7e1325057debf2cfd037c02a15f6634245
Liubov Nedoshivina <31125474+nedshivina@users.noreply.github.com>
2023-05-09 09:48:26 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from __future__ import annotations
 
 from setuptools import setup  # type: ignore
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
-setup()
+setup(long_description=long_description)
